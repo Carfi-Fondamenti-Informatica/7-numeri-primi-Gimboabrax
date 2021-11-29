a2128fd7a23dd1b849f51b1b63eef1ec10797b19
Abrammm <91841366+Gimboabrax@users.noreply.github.com>
2021-11-29 11:53:11 +0100

    Add files via upload

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+//
+// Created by Abram on 29/11/2021.
+//
 
+#include "lib.h"
+
+bool primi(int &n){
+    bool flag=false;
+    for(int i=n-1; (i<n)&(i>1); i--){
+        if((n%i)==0){
+            flag=false;
+            break;
+        } else{
+            flag=true;
+        }
+    }
+
+    return flag;
+}
--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by Abram on 29/11/2021.
+//
 
+#ifndef ESERCITAZIONE_7_LIB_H
+#define ESERCITAZIONE_7_LIB_H
+//
+bool primi(int &);
+#endif //ESERCITAZIONE_7_LIB_H
